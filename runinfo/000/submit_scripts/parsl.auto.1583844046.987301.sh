36cb19f0f7f132a480ef34a9d6cc52247df8abe5
Sohit Miglani <sohitmiglani@gmail.com>
2020-03-10 06:35:56 -0700

    improved doc

---
+++ runinfo/000/submit_scripts/parsl.auto.1583844046.987301.sh
@@ -0,0 +1,15 @@
+
+export CORES=$(getconf _NPROCESSORS_ONLN)
+echo "Found cores : $CORES"
+WORKERCOUNT=1
+
+CMD ( ) {
+process_worker_pool.py   -p 0 -c 1 -m None --poll 10 --task_url=tcp://127.0.0.1:54303 --result_url=tcp://127.0.0.1:54293 --logdir=/Users/sohit/Desktop/parallel_patterns/runinfo/000/htex_local --block_id=66 --hb_period=30 --hb_threshold=120 
+}
+for COUNT in $(seq 1 1 $WORKERCOUNT)
+do
+    echo "Launching worker: $COUNT"
+    CMD &
+done
+wait
+echo "All workers done"
